9f4eb46e35cadacbfa5b9f9976052c62ca60acc4
sreenivasa443 <114741893+sreenivasa443@users.noreply.github.com>
2022-09-30 17:01:32 +0530

    Add files via upload

---
+++ InvertString.java
@@ -0,0 +1,13 @@
+
+public class InvertString {
+
+	public static void main(String[] args) 
+	{
+		StringBuffer sb=new StringBuffer("java is oop language");
+		System.out.println(sb.reverse());
+		
+		
+       
+	}
+
+}
